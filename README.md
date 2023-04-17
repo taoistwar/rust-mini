1c84781766c9cdeb4b79108e4cabcc4eb089c8ad
ning_guanyi <taoistwar@163.com>
2023-04-17 19:58:31 +0800

    refinery sqlite demo

---
+++ README.md
@@ -0,0 +1,7 @@
+# refinery
+
+```bash
+cargo install refinery_cli
+refinery setup
+refinery migrate -c refinery.toml -p ./tests/sql_migrations
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rusqlite::Connection;
+
+mod embedded {
+    use refinery::embed_migrations;
+    embed_migrations!("./tests/sql_migrations");
+}
+
 fn main() {
-    println!("Hello Rust!");
+    let mut conn = Connection::open("./test.sqlite").unwrap();
+    embedded::migrations::runner().run(&mut conn).unwrap();
 }
