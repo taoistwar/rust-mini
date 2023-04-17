82c37f585e33b70ad4e649ca37e2a0e4f1c6f4b7
ning_guanyi <taoistwar@163.com>
2023-04-17 18:05:11 +0800

    bincode v2.0.0-rc.3 demo

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,54 @@
-use serde::{Deserialize, Serialize};
+use bincode::{config, Decode, Encode};
+use serde::{ser::SerializeStruct, Serialize};
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+static BINCODE_CONF: config::Configuration = config::standard();
+#[derive(Copy, Hash, Debug, Clone, Eq, PartialEq, Encode, Decode, PartialOrd, Ord)]
 struct Entity {
-    x: f32,
-    y: f32,
+    x: i32,
+    y: i32,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+impl Serialize for Entity {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("Window", 2)?;
+
+        state.serialize_field("x", &self.x)?;
+        state.serialize_field("y", &self.y)?;
+
+        state.end()
+    }
+}
+
+#[derive(Hash, Debug, Clone, Eq, PartialEq, Encode, Decode, PartialOrd, Ord)]
 struct World(Vec<Entity>);
 
+impl World {
+    pub fn to_bytes(&self) -> Result<Vec<u8>, bincode::error::EncodeError> {
+        bincode::encode_to_vec(self, BINCODE_CONF)
+    }
+    pub fn from_bytes(bs: &[u8]) -> Result<World, bincode::error::DecodeError> {
+        let (record, len) = bincode::decode_from_slice(bs, BINCODE_CONF)?;
+
+        if len != bs.len() {
+            return Err(bincode::error::DecodeError::ArrayLengthMismatch {
+                required: bs.len(),
+                found: len,
+            });
+        }
+
+        Ok(record)
+    }
+}
+
 fn main() {
-    let world = World(vec![Entity { x: 0.0, y: 4.0 }, Entity { x: 10.0, y: 20.5 }]);
+    let world = World(vec![Entity { x: 0, y: 4 }, Entity { x: 10, y: 20 }]);
 
-    let encoded: Vec<u8> = bincode::serialize(&world).unwrap();
+    let encoded: Vec<u8> = world.to_bytes().unwrap();
 
-    // 8 bytes for the length of the vector, 4 bytes per float.
-    assert_eq!(encoded.len(), 8 + 4 * 4);
-
-    let decoded: World = bincode::deserialize(&encoded[..]).unwrap();
+    let decoded: World = World::from_bytes(&encoded[..]).unwrap();
 
     assert_eq!(world, decoded);
 }
