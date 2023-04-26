63b721bac6fbe097a9ff3d3089b10a6a3b2d2abf
ning_guanyi <taoistwar@163.com>
2023-04-26 17:51:56 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello Rust!");
+use polars::prelude::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 创建一个DataFrame
+    let s0 = Series::new("days", [0, 1, 2].as_ref());
+    let s1 = Series::new("temp", [22.1, 19.9, 7.].as_ref());
+
+    let df: DataFrame = DataFrame::new(vec![s0, s1])?;
+    // 打印DataFrame的内容
+    println!("{}", df);
+
+    // 选择单个列
+    let days = df.column("days").unwrap();
+    println!("{}", days);
+
+    // 计算一个新的列
+    let new_days: Series = days.cast(&DataType::Float64)? * 2.5;
+    println!("{}", new_days);
+    let df = df.select(["days"])?;
+    println!("{}", df);
+    Ok(())
 }
